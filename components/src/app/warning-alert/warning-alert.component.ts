18bdc2677e9f5ff900ff7b0c3699b38ef19c3e26
aamirindi <aamirindi@gmail.com>
2024-01-17 12:20:23 +0530

    angular-first

---
+++ components/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrl: './warning-alert.component.css',
+     styles: [
+        `
+            h3{
+              padding: 20px;
+                background-color: mistyrose;
+                border: 1px solid red;
+            }
+        `
+    ]
+
+})
+export class WarningAlertComponent {
+
+}

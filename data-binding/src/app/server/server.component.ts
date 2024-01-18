02e51ec77b25043e9e7c1f4d8b7e6d84bcda7377
aamirindi <aamirindi@gmail.com>
2024-01-18 13:15:28 +0530

    data-binding

---
+++ data-binding/src/app/server/server.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.css'
+})
+export class ServerComponent {
+  // String Interpolation
+  serverId = 10;
+  // serverId: number = 10;
+  serverStatus = "offline";
+  // serverStatus: string = "offline"
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+
+  
+}
---
+++ data-binding/src/app/servers/servers.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = 'Testserver';
+
+  constructor (){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName
+  }
+  onUpdateServerName(event : Event){
+    // console.log(event)
+    this.serverName = (<HTMLInputElement>event.target).value
+  }
+}

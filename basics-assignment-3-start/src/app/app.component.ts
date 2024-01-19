fbd0464d4b604fd4b0a3e4d8e5e032a65db568a2
aamirindi <aamirindi@gmail.com>
2024-01-19 12:37:26 +0530

    assign-directives

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showPass = !this.showPass;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date())
   }
 }
 

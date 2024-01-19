a1a011334a3de05037f6e9cab48e78feef8dd1f8
aamirindi <aamirindi@gmail.com>
2024-01-19 12:31:04 +0530

    directive-assign

---
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  showPass = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showPass = !this.showPass;
+    this.log.push(this.log.length + 1);
+  }
+}
+
+

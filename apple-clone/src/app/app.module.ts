e7947397be1cc0348cc184c7f7d89f084ffb51ef
aamirindi <aamirindi@gmail.com>
2024-02-14 11:33:20 +0530

    apple-clone

---
+++ apple-clone/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+// import { SearchIcon } from '@mui/icons-material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+}

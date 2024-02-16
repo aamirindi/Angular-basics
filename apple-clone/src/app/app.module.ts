150be52ae6de97da3b2b1db07df029d3f75388c2
aamirindi <aamirindi@gmail.com>
2024-02-16 17:12:13 +0530

    apple-clone

--- apple-clone/src/app/app.module.ts
+++ apple-clone/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
-// import { SearchIcon } from '@mui/icons-material';
+import {MatIconModule} from '@angular/material/icon';
+import { PhoneComponent } from './phone/phone.component';
+import { VisionComponent } from './vision/vision.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 
 @NgModule({
@@ -11,9 +14,13 @@ import { MainComponent } from './main/main.component';
     AppComponent,
     NavbarComponent,
     MainComponent,
+    PhoneComponent,
+    VisionComponent,
+    ShoppingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
